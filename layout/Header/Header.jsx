244d6ad1db721c2ca53d63867def591c8a56319a
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-03-13 22:18:04 +0200

    refactor logo

--- layout/Header/Header.jsx
+++ layout/Header/Header.jsx
@@ -9,7 +9,10 @@ export const Header = () => {
       <Container>
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-x-[20px] md:gap-x-[40px]">
-            <Logo isClickable={true} />
+            <Logo
+              isClickable={true}
+              className="h-[24px] w-[40px] md:h-[30px] md:w-[50px] xl:h-[48px] xl:w-[80px]"
+            />
             <HeaderTitle />
           </div>
           <div className="flex gap-x-[50px]">
