08886133b7d366691ccc572b507f19463ffd2358
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-03-14 12:27:34 +0200

    create data request file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Head from 'next/head';
 import { Hero, Services } from '@/views';
-import { gql, GraphQLClient } from 'graphql-request';
 import { Career } from 'views/Career/Career';
 import { ContactUs } from 'views/ContactUs/ContactUs';
+import { DataRequest } from 'utils/request';
+
+export async function getStaticProps() {
+  const data = await DataRequest();
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
 const Home = ({ data }) => {
   return (
@@ -24,109 +33,3 @@ const Home = ({ data }) => {
 };
 
 export default Home;
-
-const query = gql`
-  query MyQuery {
-    careerSection {
-      description {
-        title
-        description
-      }
-      email
-    }
-    clientsSection {
-      sliderImages {
-        alt
-        image
-        id
-      }
-    }
-    contactSection {
-      email
-      price
-      projectTypes {
-        id
-        projectType
-      }
-    }
-    feelFreeToContactUsSection {
-      description {
-        description
-        title
-      }
-    }
-    footer {
-      address
-      email
-      socialIcons {
-        alt
-        id
-        image
-      }
-    }
-    hero {
-      sliderImages {
-        alt
-        id
-        image
-      }
-    }
-    howWeWorkSection {
-      step {
-        title
-        image
-        id
-        description
-      }
-    }
-    servicesSection {
-      service {
-        id
-        image
-        title
-        description
-      }
-    }
-    teamSection {
-      teamMember {
-        description
-        id
-        name
-        photo
-        role
-      }
-    }
-    whyChooseUsSection {
-      reason {
-        description
-        image
-        id
-        title
-      }
-    }
-    worksSection {
-      work {
-        alt
-        id
-        image
-        workType
-      }
-    }
-  }
-`;
-
-export async function getStaticProps() {
-  const endpoint = 'https://graphql.datocms.com/';
-  const graphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-      'content-type': 'application/json',
-      authorization: 'Barear ' + process.env.NEXT_DATOCMS_API_TOKEN,
-    },
-  });
-  const data = await graphQLClient.request(query);
-  return {
-    props: {
-      data,
-    },
-  };
-}
