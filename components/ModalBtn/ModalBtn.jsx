d2b45378797ee794f169a4f479b8b7893dd9a576
npakypop <leaterood@gmail.com>
2023-03-15 14:52:12 +0200

    styles for btn

--- components/ModalBtn/ModalBtn.jsx
+++ components/ModalBtn/ModalBtn.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 export const ModalBtn = ({ text, className }) => {
   return (
     <button
-      className={`inline-block border-none bg-mainBlack py-[10px] px-[33px] text-small font-bold uppercase tracking-wider text-white ${className}`}
+      className={`inline-block py-[10px] px-[33px] text-small font-bold uppercase tracking-wider ${className}`}
+      // className={`inline-block border-none bg-mainBlack py-[10px] px-[33px] text-small font-bold uppercase tracking-wider text-white ${className}`}
     >
       {text}
     </button>
