57a3c522ccb0e7e58748573d0d44fafdd3a8e6a7
Volodymyr Babych <v.d.babych@gmail.com>
2023-03-16 22:04:27 +0200

    masonry gallery and clients swiper update

--- components/MasonryGallery/MasonryGallery.jsx
+++ components/MasonryGallery/MasonryGallery.jsx
@@ -1,22 +1,13 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import Image from 'next/image';
-import css from './MansoryGallery.module.css';
+import Masonry from '@mui/lab/Masonry';
 import { convertImage, toBase64 } from 'utils/blur';
 export const MasonryGallery = ({ itemData }) => {
   return (
-    <ImageList
-      variant="masonry"
-      cols={2}
-      style={{
-        columnGap: '60px',
-        marginBottom: '-160px',
-      }}
-    >
-      {itemData.map(item => {
+    <Masonry columns={2} component="ul" className="" spacing={7.5}>
+      {itemData?.map(item => {
         return (
-          <ImageListItem key={item.url} className="group pb-[160px] ">
+          <li key={item.id} className="group relative">
             <Image
               className="h-auto w-full "
               src={item.url}
@@ -28,15 +19,46 @@ export const MasonryGallery = ({ itemData }) => {
                 convertImage(item.width, item.height),
               )}`}
             />
-            <div
-              className={`absolute left-0 top-0 bottom-[100px] z-10 w-full opacity-0 hover:opacity-[.15] ${css.overlay}`}
-            ></div>
-            <p className="absolute left-6 top-11 z-20 text-[28px] font-black leading-[34px] text-white opacity-0 group-hover:opacity-100">
+            <div className="overlay absolute left-0 top-0 bottom-[100px] z-10 h-[calc(100%-160px)] w-full opacity-[0] hover:opacity-[.15]"></div>
+            <p className="absolute left-6 top-11 z-20 text-[28px] font-black leading-[34px] text-white opacity-[0] group-hover:opacity-100">
               {item.title}
             </p>
-          </ImageListItem>
+          </li>
         );
       })}
-    </ImageList>
+    </Masonry>
+    // <ImageList
+    //   variant="masonry"
+    //   cols={2}
+    //   style={{
+    //     columnGap: '60px',
+    //     marginBottom: '-160px',
+    //   }}
+    // >
+    //   {itemData?.map((item, idx) => {
+    //     return (
+    //       <ImageListItem key={item.id} className="group pb-[160px] ">
+    //         <Image
+    //           className="h-auto w-full "
+    //           src={item.url}
+    //           alt={item.alt}
+    //           width={item.width}
+    //           height={item.height}
+    //           placeholder="blur"
+    //           blurDataURL={`data:image/svg+xml;base64,${toBase64(
+    //             convertImage(item.width, item.height),
+    //           )}`}
+    //         />
+    //         <div className="overlay absolute left-0 top-0 bottom-[100px] z-10 h-[calc(100%-160px)] w-full opacity-[1] hover:opacity-[.15]"></div>
+    //         <p className="absolute left-6 top-11 z-20 text-[28px] font-black leading-[34px] text-white opacity-[1] group-hover:opacity-100">
+    //           title-{item.title}
+    //           width-{item.width}
+    //           height-{item.height}
+    //           idx-{idx}
+    //         </p>
+    //       </ImageListItem>
+    //     );
+    //   })}
+    // </ImageList>
   );
 };
