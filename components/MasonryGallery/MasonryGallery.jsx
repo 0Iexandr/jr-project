50e56e621c7ebb480bf6e610912917194b9636ab
Volodymyr Babych <v.d.babych@gmail.com>
2023-03-17 10:50:31 +0200

    masonry gallery fixed

--- components/MasonryGallery/MasonryGallery.jsx
+++ components/MasonryGallery/MasonryGallery.jsx
@@ -4,7 +4,12 @@ import Masonry from '@mui/lab/Masonry';
 import { convertImage, toBase64 } from 'utils/blur';
 export const MasonryGallery = ({ itemData }) => {
   return (
-    <Masonry columns={2} component="ul" className="" spacing={7.5}>
+    <Masonry
+      columns={2}
+      component="ul"
+      sx={{ overflowAnchor: 'none' }}
+      spacing={7.5}
+    >
       {itemData?.map(item => {
         return (
           <li key={item.id} className="group relative">
@@ -19,7 +24,7 @@ export const MasonryGallery = ({ itemData }) => {
                 convertImage(item.width, item.height),
               )}`}
             />
-            <div className="overlay absolute left-0 top-0 bottom-[100px] z-10 h-[calc(100%-160px)] w-full opacity-[0] hover:opacity-[.15]"></div>
+            <div className="overlay absolute left-0 top-0 bottom-[0] z-10 h-full w-full opacity-[0] hover:opacity-[.15]"></div>
             <p className="absolute left-6 top-11 z-20 text-[28px] font-black leading-[34px] text-white opacity-[0] group-hover:opacity-100">
               {item.title}
             </p>
@@ -27,38 +32,5 @@ export const MasonryGallery = ({ itemData }) => {
         );
       })}
     </Masonry>
-    // <ImageList
-    //   variant="masonry"
-    //   cols={2}
-    //   style={{
-    //     columnGap: '60px',
-    //     marginBottom: '-160px',
-    //   }}
-    // >
-    //   {itemData?.map((item, idx) => {
-    //     return (
-    //       <ImageListItem key={item.id} className="group pb-[160px] ">
-    //         <Image
-    //           className="h-auto w-full "
-    //           src={item.url}
-    //           alt={item.alt}
-    //           width={item.width}
-    //           height={item.height}
-    //           placeholder="blur"
-    //           blurDataURL={`data:image/svg+xml;base64,${toBase64(
-    //             convertImage(item.width, item.height),
-    //           )}`}
-    //         />
-    //         <div className="overlay absolute left-0 top-0 bottom-[100px] z-10 h-[calc(100%-160px)] w-full opacity-[1] hover:opacity-[.15]"></div>
-    //         <p className="absolute left-6 top-11 z-20 text-[28px] font-black leading-[34px] text-white opacity-[1] group-hover:opacity-100">
-    //           title-{item.title}
-    //           width-{item.width}
-    //           height-{item.height}
-    //           idx-{idx}
-    //         </p>
-    //       </ImageListItem>
-    //     );
-    //   })}
-    // </ImageList>
   );
 };
