bd8f63feb757f288d86140a0c64c93d7e90612f5
YZarytskyi <yurazar222@gmail.com>
2023-03-20 11:55:18 +0200

    refactor

--- utils/schema.js
+++ utils/schema.js
@@ -2,8 +2,6 @@ import * as yup from 'yup';
 
 const phoneRegExp = /^\+[1-9]\d{11}$/;
 const latinRegExp = /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/g;
-const emailRegExp =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const contactFormSchema = yup.object().shape({
   name: yup
@@ -24,7 +22,6 @@ export const contactFormSchema = yup.object().shape({
     .required('*Enter your correct email!')
     .email('*Enter your correct email!')
     .matches(/^[a-zA-Z]{3}/, '*Enter your correct email!')
-    .matches(emailRegExp, '*Enter your correct email!')
     .max(63, '*Enter your correct email!'),
   projectType: yup.string().required('*Choose Project Type!'),
 });
@@ -48,6 +45,5 @@ export const modalFormSchema = yup.object().shape({
     .required('*Enter your correct email!')
     .email('*Enter your correct email!')
     .matches(/^[a-zA-Z]{3}/, '*Enter your correct email!')
-    .matches(emailRegExp, '*Enter your correct email!')
     .max(63, '*Enter your correct email!'),
 });
